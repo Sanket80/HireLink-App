6cf729f14dfdfa1e4e16040bf2e3d6a4058f58f7
Sanket80 <sanketkadamsss4@gmail.com>
2024-03-18 20:05:49 +0530

    Login Completed

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,17 +1,90 @@
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:hirelink/Screens/SignUpScreen.dart';
 import 'package:hirelink/Screens/onBoarding.dart';
 import 'package:hirelink/Widgets/text_field_input.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
-class LoginScreen extends StatelessWidget {
+import '../utils/ip.dart';
+import 'HomeScreen.dart';
+
+class LoginScreen extends StatefulWidget {
   LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
+
   final TextEditingController _passwordController = TextEditingController();
+
   bool _isLoading = false;
 
-  @override
+  late SharedPreferences prefs;
 
+  @override
+  void initState() {
+    super.initState();
+    initSharedPreferences();
+  }
+
+  void initSharedPreferences() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  void _loginUser() async {
+    try{
+      setState(() {
+        _isLoading = true;
+      });
+      if(_emailController.text.isEmpty || _passwordController.text.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Please fill all the fields'),
+            backgroundColor: Color(0xffe11d48),
+          ),
+        );
+        return;
+      }
+      var signBody = {
+        'email': _emailController.text,
+        'password': _passwordController.text,
+      };
+
+      var url = 'http://$ipAddress:$port/api/login';
+
+      var response = await http.post(Uri.parse(url),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: jsonEncode(signBody));
+
+      var jsonResponse = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        // prefs.setString('token', jsonResponse['token']);
+        // print('Token: ${jsonResponse['token']}');
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          ),
+        );
+      } else {
+        print('Error');
+      }
+    }
+    catch(e){
+      print(e);
+    }
+    setState(() {
+      _isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,106 +92,109 @@ class LoginScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(12.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Flexible(child: Container(), flex: 2),
-              Image.asset('assets/hirelink_logo.png',height: 200,),
-              SizedBox(height: 8),
-              Text(
-                'AI-Powered Talent Match, Hiring Success Unlocked',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 32),
-              TextFormField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 16),
-              TextFormField(
-                controller: _passwordController,
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Add your onPressed function here
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red, // Background color
-                    onPrimary: Colors.white, // Text color
-                    padding: EdgeInsets.symmetric(vertical: 14, horizontal: 38),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-
-
-              SizedBox(height: 16),
-              Flexible(child: Container(), flex: 2),
-
-              Row(
+          child: SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    child: Text('Don\'t have an account? '),
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  Flexible(child: Container(), flex: 1),
+                  Image.asset('assets/hirelink_logo.png',height: 200,),
+                  SizedBox(height: 8),
+                  Text(
+                    'AI-Powered Talent Match, Hiring Success Unlocked',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => onBoardingScreen(),
-                        ),);
-                    },
-                    child: Container(
-                      child: Text(
-                        'Sign up',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xffe11d48),
+                  SizedBox(height: 32),
+                  TextFormField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 12),
+                  TextFormField(
+                    controller: _passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 12),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: _loginUser,
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xffe11d48), // Background color
+                        onPrimary: Colors.white, // Text color
+                        padding: EdgeInsets.symmetric(vertical: 14, horizontal: 38),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
                       ),
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  )
-                ],
-              )
+                  ),
 
-            ],
+
+                  SizedBox(height: 32),
+                  Flexible(child: Container(), flex: 2),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: Text('Don\'t have an account? '),
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => onBoardingScreen(),
+                            ),);
+                        },
+                        child: Container(
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xffe11d48),
+                            ),
+                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        ),
+                      )
+                    ],
+                  )
+
+                ],
+              ),
+            ),
           ),
         ),
       ),
---
+++ lib/utils/ip.dart
@@ -0,0 +1,2 @@
+const String ipAddress = '192.168.0.100';
+const int port = 3000;
